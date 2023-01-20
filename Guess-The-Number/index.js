b8ec2f244ff8835ec88e0fce3dcab64bb938a001
Asdiel85 <61838617+Asdiel85@users.noreply.github.com>
2023-01-20 16:22:30 +0200

    fixed the bugs, game is working

--- Guess-The-Number/index.js
+++ Guess-The-Number/index.js
@@ -5,37 +5,41 @@ const readline = require("readline").createInterface({
 let computerGuess = Math.floor(Math.random() * 100);
 console.log(computerGuess);
 function wrongNumber(firstNumber, secondNumber) {
-  if (firstNumber > secondNumber) {
-    readline.setPrompt("Too Big \n");
+  if (firstNumber > 100 || firstNumber < 0) {
+    readline.setPrompt("Please enter a valid number \n");
+    readline.prompt();
+  } else if (isNaN(firstNumber)) {
+    readline.setPrompt("Please enter a valid number \n");
+    readline.prompt();
+  }
+   else {
+    if (firstNumber > secondNumber) {
+      readline.setPrompt("Too Big \n");
       readline.prompt();
-  } else if (firstNumber < secondNumber) {
-    readline.setPrompt("Too Small \n");
+    } else if (firstNumber < secondNumber) {
+      readline.setPrompt("Too Small \n");
       readline.prompt();
+    }
   }
 }
 let recursiveAsyncReadline = function () {
   readline.question("Guess the number (0-100): \n", (number) => {
     let playerNumber = Number(number);
 
-    if (playerNumber > 100 || playerNumber < 0) {
-      readline.setPrompt("Please enter a valid number \n");
-      readline.prompt();
-    } else if (playerNumber == computerGuess) {
+    if (playerNumber == computerGuess) {
       console.log("Nice!!!");
       readline.close();
     } else {
       wrongNumber(playerNumber, computerGuess);
       readline.on("line", (playerNumber) => {
         if (playerNumber == computerGuess) {
-          console.log("Nice!!!");
+          console.log("Congatulations you have guessed the number!!!");
           readline.close();
-        } 
-        else {
+        } else {
           wrongNumber(playerNumber, computerGuess);
-         
         }
       });
     }
   });
 };
-recursiveAsyncReadline()
+recursiveAsyncReadline();
